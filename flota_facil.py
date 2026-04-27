fa68bfeed396085e4610e9bc50fbbe39802ea8b2
Gestion-flota <andresretamalmenaa@gmail.com>
2026-04-27 14:15:17 -0400

    Update flota_facil.py

--- flota_facil.py
+++ flota_facil.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def crear_db():
  conn = sqlite3.connect('datos_vFinal.db')
     
-    c = conn.cursor()
+c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS reportes
                  (empresa TEXT, conductor TEXT, patente TEXT, guia TEXT, 
                   foto BLOB, latitud REAL, longitud REAL, fecha TEXT)''')
